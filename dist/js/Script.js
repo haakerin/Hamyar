7a43287d1b33720f988bb6cae39660e811bd051d
amirali <amirali.programer2017@gmail.com>
2023-07-08 13:22:26 +0330

    matter menu moving after reload

--- dist/js/Script.js
+++ dist/js/Script.js
@@ -27,7 +27,7 @@ form_singup.directive("compareTo", function () {
     };
 });
 var form_login = angular.module("login_form", []);
-form_login.controller("lgoinCTR" , function($scope,$http){
+form_login.controller("lgoinCTR", function ($scope, $http) {
 
 });
 
@@ -41,28 +41,35 @@ form_login.controller("lgoinCTR" , function($scope,$http){
 var Dashboard = angular.module("Dashboard", ["ngRoute"]);
 Dashboard.controller('Dashboard', function ($scope) {
 
-    $scope.change_menu = function(menu){
-       window.location.href = menu[0].url;
-        // window.location.href = "!" + menu.url;
-        // alert(menu.url);
-        document.getElementById("marker_menu").style.top = menu[1].size+"rem " ;
-        
-        document.getElementById("marker2_menu").style.top = menu[1].size +"rem " ;
-        
-        setTimeout(function (){ 
-            document.getElementById("close-menu").click()}
+    $scope.change_menu = function (menu) {
+        window.location.href = menu[0].url;
+        document.getElementById("marker_menu").style.top = menu[1].size + "rem ";
+
+        document.getElementById("marker2_menu").style.top = menu[1].size + "rem ";
+
+        setTimeout(function () {
+            document.getElementById("close-menu").click()
+        }
             , 1000);
     }
-
 });
+window.onload = (event) => {
+    var hash = window.location.hash.substring(1);
+    // console.log(hash);
+    if (hash == '!/dashboard') { document.getElementById("marker_menu").style.top = "8.1rem";console.log('kar'); document.getElementById("marker2_menu").style.top = "14.5rem"; }
+    else if (hash == '!/apps') { document.getElementById("marker_menu").style.top = "11.6rem"; document.getElementById("marker2_menu").style.top = "19.1rem"; }
+    else if (hash == '!/setting') { document.getElementById("marker_menu").style.top = "14.9rem"; document.getElementById("marker2_menu").style.top = "23.6rem"; }
+    else if (hash == '!/guid') { document.getElementById("marker_menu").style.top = "18.5rem"; document.getElementById("marker2_menu").style.top = "28.2rem"; }
+
+};
 // window.location.href = '#dashboard';
 Dashboard.config(["$routeProvider", function ($routeProvider) {
     // let location = window.location.hash.replace('#','/')
-    $routeProvider.when('/dashboard',{ templateUrl: "../../Dashboard.html" } );
-    $routeProvider.when('/select_app',{templateUrl:"../../Select_app.html"});
-    $routeProvider.when('/apps',{templateUrl:"../../Dashboard_app.html"});
-    $routeProvider.when('/guid',{templateUrl:"../../guid.html"})
-    $routeProvider.when('/setting',{templateUrl:"../../setting.html"})
+    $routeProvider.when('/dashboard', { templateUrl: "../../Dashboard.html" });
+    $routeProvider.when('/select_app', { templateUrl: "../../Select_app.html" });
+    $routeProvider.when('/apps', { templateUrl: "../../Dashboard_app.html" });
+    $routeProvider.when('/guid', { templateUrl: "../../guid.html" })
+    $routeProvider.when('/setting', { templateUrl: "../../setting.html" })
 
 
 }]);
@@ -70,30 +77,30 @@ Dashboard.config(["$routeProvider", function ($routeProvider) {
 
 var select_app = angular.module("select_app", []);
 Dashboard.controller('select_app', function ($scope) {
-    $scope.click = function(name) {
+    $scope.click = function (name) {
         // console.log(name);
         // $scope[name].toogleclass('noborder','border');
- let search_learn = document.getElementById('search_learn').value;
-//  alert(search_learn);
-        if ($scope[name] == 'noborder'){
+        let search_learn = document.getElementById('search_learn').value;
+        //  alert(search_learn);
+        if ($scope[name] == 'noborder') {
             $scope[name] = 'border';
-            if( search_learn == ''){
+            if (search_learn == '') {
                 $scope.name_learn = name;
             }
-            else{
+            else {
                 $scope.name_learn = search_learn;
 
             }
             document.getElementById('click_model').click();
         }
-      else{
-          $scope[name] = "noborder";
-        //   $scope.name_learn = ' ';
-      }
-      
+        else {
+            $scope[name] = "noborder";
+            //   $scope.name_learn = ' ';
+        }
+
     };
-    $scope.disChoies_timetable = function(){
-    location.href+="?anything#selectapp";
+    $scope.disChoies_timetable = function () {
+        location.href += "?anything#selectapp";
 
     };
 });
