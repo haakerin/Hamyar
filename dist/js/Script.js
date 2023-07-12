23929e7c7b8ef4b80c3d639ea7d6355c81c0fe07
amirali <amirali.programer2017@gmail.com>
2023-07-12 11:15:46 +0330

    add get plan add name in signup form and recode setting

--- dist/js/Script.js
+++ dist/js/Script.js
@@ -102,17 +102,9 @@ form_login.controller("lgoinCTR", function ($scope, $http) {
 
     }
 });
-
-
-
-
-
-
 // Dashboard
-
 var Dashboard = angular.module("Dashboard", ["ngRoute"]);
-Dashboard.controller('Dashboard', function ($scope) {
-
+Dashboard.controller('Dashboard', function ($scope, $http) {
     // document.getElementById("boz").style.background = "red";
     // console.log('boz');
     $scope.username_header = localStorage.getItem('username');
@@ -129,35 +121,73 @@ Dashboard.controller('Dashboard', function ($scope) {
         localStorage.removeItem('been');
         location.reload();
     }
+    function getplan() {
+        $http({
+            method: "POST",
+            url: 'https://hamyar-api.iran.liara.run/get-plans.php',
+            data: { "username": localStorage.getItem('username') },
+        }).then(function (response) {
+            console.log(response.data.message);
+            console.log(response);
+            $scope.message = response.data.message;
+            let color = [];
+
+            for(let i = 0; i < response.data.message.length; i++) {
+                let level = response.data.message[i].level;
+               console.log($scope.message[i].color);
+                if(level < 40){
+                    $scope.message[i].color='green';
+                }
+                else if(level > 60 ){
+                    $scope.message[i].color ='red';
+                }
+                else{
+                    $scope.message[i].color ='yellow';
+                }
+            }
+            console.log(color);
+            // $scope.message.color = color;
+
+        });
+    }
+
+    // console.log(progress);
+    getplan();
 });
+var n = 0
 window.onload = (event) => {
     var hash = window.location.hash.substring(1);
     // console.log(hash);
     if (hash == '!/dashboard') { document.getElementById("marker_menu").style.top = "8.1rem"; document.getElementById("marker2_menu").style.top = "10.9rem"; }
-    else if (hash == '!/apps') { document.getElementById("marker_menu").style.top = "11.6rem"; document.getElementById("marker2_menu").style.top = "17rem"; }
+    else if (hash == '!/apps') {document.getElementById("marker_menu").style.top = "11.6rem"; document.getElementById("marker2_menu").style.top = "17rem";}
     else if (hash == '!/select_app') { document.getElementById("marker_menu").style.top = "11.6rem"; document.getElementById("marker2_menu").style.top = "17rem"; }
-    else if (hash == '!/setting') { document.getElementById("marker_menu").style.top = "14.9rem"; document.getElementById("marker2_menu").style.top = "21.6rem"; }
+    else if (hash == '!/setting') { document.getElementById("marker_menu").style.top = "14.9rem"; document.getElementById("marker2_menu").style.top = "21.6rem";    }
     else if (hash == '!/guid') { document.getElementById("marker_menu").style.top = "18.5rem"; document.getElementById("marker2_menu").style.top = "26.2rem"; }
 
 };
+
 Dashboard.config(["$routeProvider", function ($routeProvider) {
     $routeProvider.when('/dashboard', { templateUrl: "../../Dashboard.html" });
     $routeProvider.when('/select_app', { templateUrl: "../../Select_app.html" });
     $routeProvider.when('/apps', { templateUrl: "../../Dashboard_app.html" });
     $routeProvider.when('/guid', { templateUrl: "../../guid.html" });
-    $routeProvider.when('/setting', { templateUrl: "../../setting.html" });
+    $routeProvider.when('/setting', { templateUrl: "../../setting.html" }
+    );
 }]);
+
 var select_app = angular.module("select_app", ["ngRoute"]);
 Dashboard.controller('select_app', function ($scope, $http) {
     $scope.click = function (name) {
         let search_learn = document.getElementById('search_learn').value;
         localStorage.setItem('subject', name[1].subject);
-        localStorage.setItem('plan_name', name[0].plan_name);
         if (search_learn == '') {
             $scope.name_learn = name[0].plan_name;
+            localStorage.setItem('plan_name', name[0].plan_name);
         }
         else {
             $scope.name_learn = search_learn;
+            localStorage.setItem('plan_name', search_learn);
+
         }
         document.getElementById('click_model').click();
     };
@@ -165,6 +195,7 @@ Dashboard.controller('select_app', function ($scope, $http) {
         location.href += "?anything#selectapp";
     };
     $scope.send_timetable = function () {
+
         let saturday = ['saturday1', 'saturday2', 'saturday3', 'saturday4', 'saturday5', 'saturday6', 'saturday7', 'saturday8'];
         let sunday = ['sunday1', 'sunday2', 'sunday3', 'sunday4', 'sunday5', 'sunday6', 'sunday7', 'sunday8'];
         let monday = ['monday1', 'monday2', 'monday3', 'monday4', 'monday5', 'monday6', 'monday7', 'monday8'];
@@ -172,7 +203,7 @@ Dashboard.controller('select_app', function ($scope, $http) {
         let wednesday = ['wednesday1', 'wednesday2', 'wednesday3', 'wednesday4', 'wednesday5', 'wednesday6', 'wednesday7', 'wednesday8'];
         let thursday = ['thursday1', 'thursday2', 'thursday3', 'thursday4', 'thursday5', 'thursday6', 'thursday7', 'thursday8'];
         let friday = ['friday1', 'friday2', 'friday3', 'friday4', 'friday5', 'friday6', 'friday7', 'friday8'];
-        let week = ['saturday','sunday','monday','tuesday','wednesday','thursday','friday'];
+        let week = ['saturday', 'sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday'];
         // for (let q = 0,v = 0; q < week.length,v < 8; q++,v++) {
         //     console.log((week[q]) );
         // }
@@ -202,7 +233,7 @@ Dashboard.controller('select_app', function ($scope, $http) {
         let TFtrusday = [];
         let TFfriday = [];
 
-        for (let o = 1,j = 0; o <= 24; o++) {
+        for (let o = 1, j = 0; o <= 24; o++) {
             TFsaturday.push(bennTF0[j]);
             TFsunday.push(bennTF1[j]);
             TFmonday.push(bennTF2[j]);
@@ -210,36 +241,49 @@ Dashboard.controller('select_app', function ($scope, $http) {
             TFwednesday.push(bennTF4[j]);
             TFtrusday.push(bennTF5[j]);
             TFfriday.push(bennTF6[j]);
-            if(o % 3 === 0) j++;
+            if (o % 3 === 0) j++;
         }
-        let user_time = {saturday : TFsaturday,sunday:TFsunday,monday:TFmonday,tuesday:TFtrusday,wednesday:TFwednesday,thursday:TFtrusday,friday:TFfriday};
-        let data = { username: localStorage.getItem('username'), subject: localStorage.getItem('subject') ,plan_name : localStorage.getItem('plan_name'),user_times : user_time};
+        let user_time = { saturday: TFsaturday, sunday: TFsunday, monday: TFmonday, tuesday: TFtrusday, wednesday: TFwednesday, thursday: TFtrusday, friday: TFfriday };
+        let data = { username: localStorage.getItem('username'), subject: localStorage.getItem('subject'), plan_name: localStorage.getItem('plan_name'), user_times: user_time };
         document.getElementById("loading").classList.add("d-flex");
         document.getElementById("loading").classList.remove("d-none");
         $http({
             method: "POST",
             url: 'https://hamyar-api.iran.liara.run/add-plan.php',
-            data:data ,
+            data: data,
         }).then(function (response) {
             if (response.data.status == -2) {
                 $scope.error = response.data.message;
-                console.log(response);
+                // console.log(response);
                 loading_none();
             } else if (response.data.status == 1) {
-                location.href = "Dashboard_base.html#!/apps";
-                location.reload();
                 loading_none();
-            }            
+                // document.body.style.background = 'green';
+                setTimeout(() => {
+                    location.href = "Dashboard_base.html#!/apps";
+                    location.reload();
+                }, 1);
+            }
         });
-        
+
         // console.log(data);
     }
-
 });
 // Dashboard app
 var Dashboard_app = angular.module("dashboard_app", []);
 Dashboard_app.controller('dashboardapp', function ($scope) {
-    document.getElementById('boz').style.background = 'red';
-    console.log('boz');
-    $scope.boz = 'boz';
 });
+// Setting
+var Setting = angular.module("Setting", []);
+Dashboard.controller('SettingCTR', function ($scope) {
+    function getdatasetting(){
+        document.getElementById("input_name_setting").value = localStorage.getItem('username');
+        document.getElementById('input_username_setting').value = localStorage.getItem('username');
+        document.getElementById('input_email_setting').value = localStorage.getItem('email');
+    }
+    getdatasetting();
+});
+
+
+
+
